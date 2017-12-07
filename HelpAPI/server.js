d2330341b8d20411814f284dc9bf209a2ce3051d
Andrei <andrei.tudor27@gmail.com>
2017-12-07 14:36:13 +0100

    CORS

--- HelpAPI/server.js
+++ HelpAPI/server.js
@@ -13,6 +13,12 @@ mongoose.connect('mongodb://localhost/HelpAPI', {
 	/* other options */
 })
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
